c6fca2b5799b436bdd1c3e5458bfcda5d9f45d51
susilo hartomo <encusilo545110@gmail.com>
2020-08-06 23:27:38 +0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useContext, createContext, useLayoutEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { useObserver, Provider, useLocalStore } from 'mobx-react'
+import { useObserver } from 'mobx-react'
 
 //pages
 import Home from './pages/Home'
@@ -9,7 +8,9 @@ import Home from './pages/Home'
 function App() {
   return useObserver(() => (
     <main>
-      <p>App</p>
+      <h1 style={{ textAlign: 'center' }}>Conuter App</h1>
+      <p style={{ textAlign: 'center', color: 'grey' }}>with</p>
+      <h4 style={{ textAlign: 'center' }}>MobX management state and React Framework</h4>
       <Home />
     </main>
   ));
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
-import React, { useContext } from 'react'
+import React from 'react'
+
+// reactive or use "observer"
+import { useObserver } from 'mobx-react'
+
+//get store
 import { useStores } from '../hooks/storeHook'
 
 export default function Home() {
     const { counter } = useStores()
-    return (
-        <div>
-            <h1>Home Page</h1>
-            {counter.count}
-        </div>
-    )
+    return useObserver(() => (
+        <>
+            {/* <div style={{ width: '100vw', height: '500px', backgroundColor: 'GrayText' }}> */}
+                <div style={{ display: "grid",width: '100vw', height: '500px',justifyContent: "center", alignItems: "center" }}>
+                    <h1>{counter.count}</h1>
+                    <button onClick={() => counter.increment()}>++</button>
+                    <button onClick={() => counter.decrement()}>--</button>
+                </div>
+            {/* </div> */}
+        </>
+    ))
 }
-  
